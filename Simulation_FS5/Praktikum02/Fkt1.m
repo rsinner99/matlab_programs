42523bb42d52ed871ba15ba8afa1b99d1adcf9fe
Robin Sinner <62691050+rsinner99@users.noreply.github.com>
2020-10-26 14:45:09 +0100

    Add files via upload

---
+++ Simulation_FS5/Praktikum02/Fkt1.m
@@ -0,0 +1,7 @@
+function y = Fkt1(x)
+    if x<2
+        y = -x./2;
+    else
+        y = x-3;
+    end
+end
---
+++ Simulation_FS5/Praktikum02/Fkt2.m
@@ -0,0 +1,9 @@
+function y = Fkt2(x)
+    if x<-1
+        y = -1;
+    elseif (-1<=x)&&(x<=2)
+            y = x;
+    else
+            y = 4-x;
+    end
+end
